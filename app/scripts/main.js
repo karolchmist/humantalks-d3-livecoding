cb149aabea3a71252efff7c2f2e729a776f5c522
Karol Chmist <karolchmist@gmail.com>
2014-05-11 21:41:16 +0200

    Simple random bar graph d3 demo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,31 @@
-console.log('\'Allo \'Allo!');
+var width = 960,
+    height = 500;
+
+var random=function(max) {
+    return Math.round(max * Math.random());
+};
+
+var data = [];
+for(var i = 0; i<20; i++) {
+    data.push(random(30));
+}
+
+var scale = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 500]);
+
+var content = d3.select("body")
+    .select("div#content");
+
+var bars = content.selectAll("div")
+    .data(data)
+    ;
+
+    bars.enter()
+        .append("div")
+        .text(function(d){return d;})
+        .style("background-color", "red")
+        .style("width", function(d){return scale(d) + "px";})
+    ;
+
+    bars.exit().remove();
